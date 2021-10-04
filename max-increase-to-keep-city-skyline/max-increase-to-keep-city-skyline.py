00ed6c91b6a6af2038eca53d364fb2fc98e8809a
nayoung <69383548+NayoungLee-de@users.noreply.github.com>
2021-10-04 21:40:10 +0900

    Time: 72 ms (79.98%), Space: 14.3 MB (55.52%) - LeetHub

--- max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.py
+++ max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.py
@@ -1,18 +1,9 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        west = []
-        south = []
-        for i in grid:
-            west.append(max(i))
+        west = [max(i) for i in grid]
+        south = [max(i) for i in zip(*grid)]
         
         n = len(grid[0])
-        
-        for i in range(n):
-            m = 0
-            for j in range(n):
-                m = max(m, grid[j][i])
-            south.append(m)
-        
         ans = 0
         for i in range(n):
             for j in range(n):
@@ -21,5 +12,3 @@ class Solution:
                 if a>=0 and b>=0:
                     ans += min(a, b)
         return ans
-                
-                
